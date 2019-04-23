b69ee8ce14f1be626040d9ac414ca93d6c2e10cc
jeong-yeeun <jeong-yeeun@172.17.196.15>
2019-04-23 23:44:01 +0900

    Lab11

---
+++ src/test/java/edu/handong/csee/java/numbers/RandomNumberGenerator.java
@@ -0,0 +1,28 @@
+package edu.handong.csee.java.numbers;
+
+public class RandomNumberGenerator{
+    private int firstNumber;
+    private int secondNumber;
+    private int randomNumber;
+    
+    RandomNumberGenerator(int n1,int n2){
+        firstNumber = n1;
+        secondNumber = n2;
+    }
+
+    void generateRandomNumber(){
+        randomNumber = (int)((Math.random() * (secondNumber - firstNumber + 1)) + firstNumber);
+    }
+    int getRandomNumber(){
+        return randomNumber;
+    }
+
+    public static void main(String[] args) {
+        int result;
+        RandomNumberGenerator rng = new RandomNumberGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        rng.generateRandomNumber();
+        result = rng.getRandomNumber();
+        System.out.println(result);
+        System.out.println("⇒ Any number between "+Integer.parseInt(args[0])+" and "+Integer.parseInt(args[1])+" will be printed.");
+    }
+}
