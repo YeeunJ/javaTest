fe6097b77f6d2505d2d9d9c681d6a93c75753b07
jeong-yeeun <jeong-yeeun@172.17.196.120>
2019-04-25 16:50:58 +0900

    hw2 2

--- src/test/java/edu/handong/csee/java/hw2/CalculatorForFourArithmeticOperators.java
+++ src/test/java/edu/handong/csee/java/hw2/CalculatorForFourArithmeticOperators.java
@@ -23,45 +23,46 @@ public class CalculatorForFourArithmeticOperators implements Calculator {
 
 	/** 
 	*This method has the function to add.
-					      *The input of this method is entered two numbers, and the output is to add two values.
-						      **/
-			    public int sum(int firstNumber,int secondNumber) {
-					        numberOfArithmeticOperatorProcessed++;
-							        return firstNumber+secondNumber;
-									    }
+	*The input of this method is entered two numbers, and the output is to add two values.
+	**/
+	public int sum(int firstNumber,int secondNumber) {
+		numberOfArithmeticOperatorProcessed++;
+		return firstNumber+secondNumber;
+	}
 
-				    /** 
-					      *This method has the function to subtract.
-						      *The input of this method is entered two numbers, and the output is to subtract the second input from the first input value.
-							      **/
-				    public int subtract(int firstNumber,int secondNumber) {
-						        numberOfArithmeticOperatorProcessed++;
-								        return firstNumber-secondNumber;
-										    }
+	/** 
+	*This method has the function to subtract.
+	*The input of this method is entered two numbers, and the output is to subtract the second input from the first input value.
+	**/
+	public int subtract(int firstNumber,int secondNumber) {
+		numberOfArithmeticOperatorProcessed++;
+		return firstNumber-secondNumber;
+		}
 
-					    /** 
-						      *This method has the function to divide.
-							      *The input of this method is entered two numbers, and the output is to divide the second input from the first input value.
-								      **/
-					    public int divide(int firstNumber,int secondNumber) {
-							        numberOfArithmeticOperatorProcessed++;
-									        return firstNumber/secondNumber;
-											    }
+	/** 
+	*This method has the function to divide.
+	*The input of this method is entered two numbers, and the output is to divide the second input from the first input value.
+	**/
+	public int divide(int firstNumber,int secondNumber) {
+		numberOfArithmeticOperatorProcessed++;
+		return firstNumber/secondNumber;
+	}
 
-						    /** 
-							      *This method has the function to multiply.
-								      *The input of this method is entered two numbers, and the output is to multiply two values.
-									      **/
-						    public int multiply(int firstNumber,int secondNumber) {
-								        numberOfArithmeticOperatorProcessed++;
-										        return firstNumber*secondNumber;
-												    }
+	/** 
+	*This method has the function to multiply.
+	*The input of this method is entered two numbers, and the output is to multiply two values.
+	**/
+	public int multiply(int firstNumber,int secondNumber) {
+		numberOfArithmeticOperatorProcessed++;
+		return firstNumber*secondNumber;
+	}
 
-							    /** 
-								      *This method counts how many display number of arithmetic operator processed.
-									      *This method doesn't need input and output value.
-										      **/
-							    public void displayNumberOfArithmeticOperatorProcessed(){
-									        System.out.println("The number of arithmetic operators processed!: " + numberOfArithmeticOperatorProcessed);
-											    }
+	/** 
+	*This method counts how many display number of arithmetic operator processed.
+	*This method doesn't need input and output value.
+	**/
+	public void displayNumberOfArithmeticOperatorProcessed(){
+		System.out.println("The number of arithmetic operators processed!: " + numberOfArithmeticOperatorProcessed);
+	}
+
 }
