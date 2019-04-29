ae8e034c11ff6255fc1ae7de787861ceab2f2b8e
YeeunJ <21800669@handong.edu>
2019-04-29 23:44:02 +0900

    finalcommit

--- src/test/java/edu/handong/csee/java/hw2/Calculator.java
+++ src/test/java/edu/handong/csee/java/hw2/Calculator.java
@@ -2,8 +2,8 @@ package edu.handong.csee.java.hw2;
 
 
 interface Calculator {
-	public int sum(int firstNumber, int secondNumber);
-	public int subtract(int firstNumber, int secondNumber);
-	public int multiply(int firstNumber, int secondNumber);
-	public int divide(int firstNumber, int secondNumber);
+	public int sum(int firstNumber,int secondNumber);
+	public int subtract(int firstNumber,int secondNumber);
+	public int multiply(int firstNumber,int secondNumber);
+	public int divide(int firstNumber,int secondNumber);
 }
